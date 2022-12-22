84781237f4762cd302fd13d0fe027b9f6ff0cac6
Nenad Bozinovic <nesaboz@gmail.com>
2022-12-21 17:26:48 -0800

    Initial notes.

--- cheat_sheet/_modidx.py
+++ cheat_sheet/_modidx.py
@@ -5,4 +5,4 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://nesaboz.github.io',
                 'git_url': 'https://github.com/nesaboz/cheat-sheet',
                 'lib_path': 'cheat_sheet'},
-  'syms': {'cheat_sheet.core': {'cheat_sheet.core.foo': ('core.html#foo', 'cheat_sheet/core.py')}}}
+  'syms': {'cheat_sheet.jupyter_nb_builder': {}}}
